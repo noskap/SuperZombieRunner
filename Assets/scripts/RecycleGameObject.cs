0edc922e6c92781f63a700dd9329c82a44ca2303
Tom <otacon@live.com.au>
2018-01-13 21:29:14 +1000

    Add project files.

---
+++ Assets/scripts/RecycleGameObject.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IRecycle
+{
+    void Restart();
+    void ShutDown();
+}
+
+public class RecycleGameObject : MonoBehaviour {
+
+    private List<IRecycle> recycleComponents;
+
+    public void Awake()
+    {
+        var components = GetComponents<MonoBehaviour>();
+        recycleComponents = new List<IRecycle>();
+        foreach (var component in components)
+        {
+            if(component is IRecycle)
+            {
+                recycleComponents.Add(component as IRecycle);
+            }
+        }
+
+        //Debug.Log(name + " Found " + recycleComponents.Count + " Components");
+    }
+
+	public void Restart()
+    {
+        gameObject.SetActive(true);
+
+        foreach (var component in recycleComponents)
+        {
+            component.Restart();
+        }
+    }
+
+    public void ShutDown()
+    {
+        gameObject.SetActive(false);
+        foreach (var component in recycleComponents)
+        {
+            component.ShutDown();
+        }
+    }
+}
